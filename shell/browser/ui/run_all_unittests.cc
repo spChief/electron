82ca97165d6c32e6076395bb5c42b49f3831fa30
John Kleinschmidt <jkleinsc@github.com>
2020-01-28 10:57:40 -0500

    Wrap LoadLibrary in CHECK

--- shell/browser/ui/run_all_unittests.cc
+++ shell/browser/ui/run_all_unittests.cc
@@ -9,7 +9,8 @@
 
 int main(int argc, char** argv) {
 #if defined(OS_WIN)
-  HMODULE m_hCommCtrlDLL = ::LoadLibrary(L"comctl32.dll");
+  // Eagerly load comctl32.dll since tests may need it or its dependents.
+  CHECK(::LoadLibrary(L"comctl32.dll"));
 #endif  // defined(OS_WIN)
   base::TestSuite test_suite(argc, argv);
   return base::LaunchUnitTests(
